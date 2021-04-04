e975d7bbe268546f20b8b84986811acee34b8740
Dan Morse <44938476+danmorse314@users.noreply.github.com>
2021-04-04 09:42:22 -0700

    Add files via upload

--- App.R
+++ App.R
@@ -19,17 +19,103 @@ tc <- read_csv("https://raw.githubusercontent.com/danmorse314/top-chef-fantasy/m
 
 tc[is.na(tc)] <- 0
 
-# just use for now while there hasn't yet been a top-3 quickfire or a LCK
-tc <- tc %>%
-  mutate(
-    qf_top3 = 0,
-    lck_win = 0,
-    lck_loss = 0
-  )
-
 team_colors <- c(Zach = "#7d826e", Dan = "#d3b140", Chris = "#cdced1")
 
 `%not_in%` <- purrr::negate(`%in%`)
+
+standings <- tc %>%
+  mutate(team_url = paste("<img src = ' ",team_url,"' height = '45'></img>")) %>%
+  group_by(team_url, team) %>%
+  summarize(
+    chefs_remaining = 5 - sum(eliminated),
+    lck_chefs = sum(status == "LCK"),
+    points = sum(weekly_points),
+    elim_wins = sum(elim_win),
+    elim_top3s = sum(elim_top3),
+    qf_wins = sum(qf_win),
+    qf_top3s = sum(qf_top3),
+    .groups = "drop"
+  ) %>%
+  ungroup() %>%
+  arrange(-points)
+
+current_week <- max(tc$week)
+
+elim_chefs <- tc %>%
+  filter(eliminated == 1) %>%
+  mutate(last_week = week) %>%
+  select(cheftestant, last_week)
+
+final_week <- tc %>%
+  filter(cheftestant %not_in% elim_chefs$cheftestant) %>%
+  group_by(cheftestant) %>%
+  summarize(last_week = max(week), .groups = "drop") %>%
+  ungroup() %>%
+  bind_rows(elim_chefs)
+
+chefs <- tc %>%
+  left_join(final_week, by = c("cheftestant")) %>%
+  filter(week == last_week) %>%
+  select(cheftestant, status, last_week) %>%
+  distinct()
+
+chefstats <- tc %>%
+  mutate(chef_url = paste("<img src = ' ",chef_url,"' height = '45'></img>")) %>%
+  mutate(team_url = paste("<img src = ' ",team_url,"' height = '45'></img>")) %>%
+  group_by(chef_url, cheftestant, team_url, team) %>%
+  summarize(
+    points = sum(weekly_points),
+    elim_wins = sum(elim_win),
+    elim_top3s = sum(elim_top3),
+    qf_wins = sum(qf_win),
+    qf_top3s = sum(qf_top3),
+    .groups = "drop"
+  ) %>%
+  ungroup() %>%
+  arrange(-points) %>%
+  left_join(chefs, by = "cheftestant") %>%
+  arrange(status) %>%
+  select(chef_url, cheftestant, status, everything())
+
+my_colors <- chefs %>%
+  mutate(
+    color = case_when(
+      status == "LCK" ~ "orange",
+      status == "ELIMINATED" ~ "red"
+    )
+  )
+
+team_weekly <- tc %>%
+  group_by(team, team_url, week) %>%
+  summarize(
+    weekly_points = sum(weekly_points),
+    .groups = "drop"
+  ) %>%
+  ungroup() %>%
+  group_by(team_url) %>%
+  mutate(cumpoints = cumsum(weekly_points)) %>%
+  ungroup() %>%
+  mutate(
+    week = case_when(
+      team == "Zach" ~ week - .2,
+      team == "Dan" ~ week,
+      team == "Chris" ~ week + .2
+    )
+  )
+
+chefbar <- tc %>%
+  group_by(chef_url, cheftestant) %>%
+  summarize(
+    points = sum(weekly_points),
+    .groups = "drop"
+  ) %>%
+  ungroup() %>%
+  arrange(-points) %>%
+  left_join(
+    distinct(select(tc, cheftestant, team)),
+    by = "cheftestant"
+  ) %>%
+  separate(cheftestant, into = c("first_name","last_name"), sep = " ", remove = FALSE)
 
 # example of plot by week
 #tc %>%
@@ -94,22 +180,6 @@ server <- function(input, output, session){
   
   output$standings <- DT::renderDataTable({
     
-    standings <- tc %>%
-      mutate(team_url = paste("<img src = ' ",team_url,"' height = '45'></img>")) %>%
-      group_by(team_url, team) %>%
-      summarize(
-        chefs_remaining = 5 - sum(eliminated),
-        lck_chefs = sum(status == "LCK"),
-        points = sum(weekly_points),
-        elim_wins = sum(elim_win),
-        elim_top3s = sum(elim_top3),
-        qf_wins = sum(qf_win),
-        qf_top3s = sum(qf_top3),
-        .groups = "drop"
-      ) %>%
-      ungroup() %>%
-      arrange(-points)
-    
     DT::datatable(
       standings,
       colnames = c(
@@ -123,22 +193,22 @@ server <- function(input, output, session){
         "Quickfire Wins" = "qf_wins",
         "Quickfire Top 3s" = "qf_top3s"
       ),
-     escape = FALSE,
-     callback = JS("
+      escape = FALSE,
+      callback = JS("
     table.on( 'order.dt search.dt', function () {
         table.column(0, {search:'applied', order:'applied'}).nodes().each( 
             function (cell, i) {
                 cell.innerHTML = i+1;
             } );
     } )"),
-     selection = "single",
-     options = list(
-       dom = "t",
-       columnDefs = list(
-         list(className = "dt-center", targets = seq(1,9,1)),
-         list(orderable = FALSE, targets = c(1,2))
-         )
-       )
+      selection = "single",
+      options = list(
+        dom = "t",
+        columnDefs = list(
+          list(className = "dt-center", targets = seq(1,9,1)),
+          list(orderable = FALSE, targets = c(1,2))
+        )
+      )
     )
     
   })
@@ -148,22 +218,6 @@ server <- function(input, output, session){
     s <- input$standings_rows_selected
     
     if(length(s)){
-      
-      standings <- tc %>%
-        mutate(team_url = paste("<img src = ' ",team_url,"' height = '55'></img>")) %>%
-        group_by(team_url, team) %>%
-        summarize(
-          chefs_remaining = 5 - sum(eliminated),
-          lck_chefs = sum(status == "LCK"),
-          points = sum(weekly_points),
-          elim_wins = sum(elim_win),
-          elim_top3s = sum(elim_top3),
-          qf_wins = sum(qf_win),
-          qf_top3s = sum(qf_top3),
-          .groups = "drop"
-        ) %>%
-        ungroup() %>%
-        arrange(-points)
       
       team_name <- standings[s, , drop = FALSE] %>%
         pull(team)
@@ -186,76 +240,16 @@ server <- function(input, output, session){
     
     if(length(s)){
       
-      standings <- tc %>%
-        mutate(team_url = paste("<img src = ' ",team_url,"' height = '55'></img>")) %>%
-        group_by(team_url, team) %>%
-        summarize(
-          chefs_remaining = 5 - sum(eliminated),
-          lck_chefs = sum(status == "LCK"),
-          points = sum(weekly_points),
-          elim_wins = sum(elim_win),
-          elim_top3s = sum(elim_top3),
-          qf_wins = sum(qf_win),
-          qf_top3s = sum(qf_top3),
-          .groups = "drop"
-        ) %>%
-        ungroup() %>%
-        arrange(-points)
-      
       team_name <- standings[s, , drop = FALSE] %>%
         pull(team)
       
-      current_week <- max(tc$week)
-      
-      elim_chefs <- tc %>%
-        filter(eliminated == 1) %>%
-        mutate(last_week = week) %>%
-        select(cheftestant, last_week)
-      
-      final_week <- tc %>%
-        filter(cheftestant %not_in% elim_chefs$cheftestant) %>%
-        group_by(cheftestant) %>%
-        summarize(last_week = max(week), .groups = "drop") %>%
-        ungroup() %>%
-        bind_rows(elim_chefs)
-      
-      chefs <- tc %>%
-        left_join(final_week, by = c("cheftestant")) %>%
-        filter(week == last_week) %>%
-        select(cheftestant, status, last_week) %>%
-        distinct()
-      
-      chefstats <- tc %>%
-        mutate(chef_url = paste("<img src = ' ",chef_url,"' height = '45'></img>")) %>%
-        mutate(team_url = paste("<img src = ' ",team_url,"' height = '45'></img>")) %>%
-        group_by(chef_url, cheftestant, team_url, team) %>%
-        summarize(
-          points = sum(weekly_points),
-          elim_wins = sum(elim_win),
-          elim_top3s = sum(elim_top3),
-          qf_wins = sum(qf_win),
-          qf_top3s = sum(qf_top3),
-          .groups = "drop"
-        ) %>%
-        ungroup() %>%
-        arrange(-points) %>%
-        left_join(chefs, by = "cheftestant") %>%
-        arrange(status) %>%
-        select(chef_url, cheftestant, status, everything()) %>%
+      chefstatsi <- chefstats %>%
         select(-team_url) %>%
         filter(team == team_name) %>%
         select(-team)
       
-      my_colors <- chefs %>%
-        mutate(
-          color = case_when(
-            status == "LCK" ~ "orange",
-            status == "ELIMINATED" ~ "red"
-          )
-        )
-      
       DT::datatable(
-        chefstats,
+        chefstatsi,
         colnames = c(
           " " = "chef_url",
           "Chef" = "cheftestant",
@@ -295,61 +289,15 @@ server <- function(input, output, session){
   
   output$chefstatshead <- renderUI({
     div(
-      br(),
+      hr(),
       h2("Cheftestant Season Stats")
     )
   })
   
   output$chefstats <- DT::renderDataTable({
     
-    current_week <- max(tc$week)
-    
-    elim_chefs <- tc %>%
-      filter(eliminated == 1) %>%
-      mutate(last_week = week) %>%
-      select(cheftestant, last_week)
-    
-    final_week <- tc %>%
-      filter(cheftestant %not_in% elim_chefs$cheftestant) %>%
-      group_by(cheftestant) %>%
-      summarize(last_week = max(week), .groups = "drop") %>%
-      ungroup() %>%
-      bind_rows(elim_chefs)
-    
-    chefs <- tc %>%
-      left_join(final_week, by = c("cheftestant")) %>%
-      filter(week == last_week) %>%
-      select(cheftestant, status, last_week) %>%
-      distinct()
-    
-    chefstats <- tc %>%
-      mutate(chef_url = paste("<img src = ' ",chef_url,"' height = '45'></img>")) %>%
-      mutate(team_url = paste("<img src = ' ",team_url,"' height = '45'></img>")) %>%
-      group_by(chef_url, cheftestant, team_url) %>%
-      summarize(
-        points = sum(weekly_points),
-        elim_wins = sum(elim_win),
-        elim_top3s = sum(elim_top3),
-        qf_wins = sum(qf_win),
-        qf_top3s = sum(qf_top3),
-        .groups = "drop"
-      ) %>%
-      ungroup() %>%
-      arrange(-points) %>%
-      left_join(chefs, by = "cheftestant") %>%
-      arrange(status) %>%
-      select(chef_url, cheftestant, team_url, status, everything())
-    
-    my_colors <- chefs %>%
-      mutate(
-        color = case_when(
-          status == "LCK" ~ "orange",
-          status == "ELIMINATED" ~ "red"
-        )
-      )
-    
     DT::datatable(
-      chefstats,
+      select(chefstats, -team),
       colnames = c(
         " " = "chef_url",
         "Chef" = "cheftestant",
@@ -375,7 +323,7 @@ server <- function(input, output, session){
         dom = "t",
         columnDefs = list(
           list(className = "dt-center", targets = seq(1,10,1)),
-          list(orderable = FALSE, targets = c(1,2,3,4))
+          list(orderable = FALSE, targets = c(1,2,3))
         ),
         pageLength = 15
       )
@@ -389,24 +337,6 @@ server <- function(input, output, session){
   
   output$teamplot <- renderPlot({
     
-    team_weekly <- tc %>%
-      group_by(team, team_url, week) %>%
-      summarize(
-        weekly_points = sum(weekly_points),
-        .groups = "drop"
-      ) %>%
-      ungroup() %>%
-      group_by(team_url) %>%
-      mutate(cumpoints = cumsum(weekly_points)) %>%
-      ungroup() %>%
-      mutate(
-        week = case_when(
-          team == "Zach" ~ week - .2,
-          team == "Dan" ~ week,
-          team == "Chris" ~ week + .2
-        )
-      )
-  
     team_weekly %>%
       ggplot(aes(week, cumpoints)) +
       geom_line(aes(color = team), show.legend = FALSE, size = 2) +
@@ -426,20 +356,6 @@ server <- function(input, output, session){
   })
   
   output$chefplot <- renderPlot({
-    
-    chefbar <- tc %>%
-      group_by(chef_url, cheftestant) %>%
-      summarize(
-        points = sum(weekly_points),
-        .groups = "drop"
-      ) %>%
-      ungroup() %>%
-      arrange(-points) %>%
-      left_join(
-        distinct(select(tc, cheftestant, team)),
-        by = "cheftestant"
-      ) %>%
-      separate(cheftestant, into = c("first_name","last_name"), sep = " ", remove = FALSE)
     
     chefbar %>%
       ggplot(aes(points, reorder(first_name, points))) +
